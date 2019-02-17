04b47ac17899700f4d8b4cceb4d326986e315d4a
Vladimir <vladriordan@gmail.com>
2019-02-17 22:16:29 +0200

    credit card form improved

--- src/components/CreditCardForm.vue
+++ src/components/CreditCardForm.vue
@@ -1,223 +1,166 @@
 <template>
-  <form
-    id="CreditCardForm"
-    @submit.prevent="handleSubmit"
-    action=""
-    method="post"
-  >
-    <div class="col-md-10">
-      <div class="row">
-        <div class="form-group">
-          <label for="cardNumber">Card Number</label>
-          <input
-            type="text"
-            v-model="card.cardNumber"
-            v-validate="{ required: true, min: 19, credit_card: true }"
-            v-mask="masks.cardNumber"
-            class="form-control"
-            name="cardNumber"
-            id="cardNumber"
-            placeholder="1234 5678 9123 4567"
-            :class="{ 'is-invalid': submitted && errors.has('cardNumber') }"
-          />
-          <div
-            v-if="submitted && errors.has('cardNumber')"
-            class="invalid-feedback"
-          >
-            {{ errors.first("cardNumber") }}
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="cardHolder">Cardholder Name</label>
-          <input
-            type="text"
-            class="form-control"
-            name="cardHolder"
-            id="cardHolder"
-            v-model="card.cardHolder"
-            v-validate="{ required: true, alpha_spaces: true }"
-            v-mask="masks.cardHolder"
-            placeholder="Sanjana Singh"
-            :class="{ 'is-invalid': submitted && errors.has('cardHolder') }"
-          />
-          <div
-            v-if="submitted && errors.has('cardHolder')"
-            class="invalid-feedback"
-          >
-            {{ errors.first("cardHolder") }}
-          </div>
-        </div>
-      </div>
-    </div>
-    <img
-      v-bind:src="creditCardImage"
-      v-bind:alt="creditCardAlt"
-      class="visa-logo"
-    />
-    <div class="row">
-      <div class="col-md-6">
-        <div class="form-group">
-          <label for="DateMonth">Expiration</label>
-          <div class="row">
-            <div class="col-md-6">
-              <input
-                v-model="card.cardDateMonth"
-                class="form-control"
-                name="DateMonth"
-                id="DateMonth"
-                v-validate="{ required: true, verify_day_and_month: true, verify_date: true }"
-                v-mask="masks.cardDateMonth"
-                placeholder="01/12"
-                :class="{
-                  'is-invalid': submitted && errors.has('DateMonth')
-                }"
-              />
-              <div
-                v-if="submitted && errors.has('DateMonth')"
-                class="invalid-feedback"
-              >
-                {{ errors.first("DateMonth") }}
-              </div>
-            </div>
-            <div class="col-md-6">
-              <input
-                v-model="card.cardYear"
-                class="form-control"
-                name="cardYear"
-                id="cardYear"
-                v-validate="{ required: true, verify_year: true, verify_date:true }"
-                v-mask="masks.cardYear"
-                placeholder="2019"
-                :class="{
-                  'is-invalid': submitted && errors.has('cardYear')
-                }"
-              />
-              <div
-                v-if="submitted && errors.has('cardYear')"
-                class="invalid-feedback"
-              >
-                {{ errors.first("cardYear") }}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="form-group">
-          <label for="cvv" class="cvv">CVV</label>
-          <input
-            type="text"
-            v-model="card.cardCVV"
-            v-validate="{ required: true, digits: 3 }"
-            v-mask="masks.cardCVV"
-            class="form-control"
-            name="cardCVV"
-            id="cvv"
-            placeholder="123"
-            :class="{ 'is-invalid': submitted && errors.has('cardCVV') }"
-          />
-          <div
-            v-if="submitted && errors.has('cardCVV')"
-            class="invalid-feedback"
-          >
-            {{ errors.first("cardCVV") }}
-          </div>
-        </div>
-      </div>
-      <div class="col-md-2">
+    <form
+            id="CreditCardForm"
+            @submit.prevent="handleSubmit"
+            action=""
+            method="post"
+    >
+        <img
+                v-bind:src="creditCardImage"
+                v-bind:alt="creditCardAlt"
+                class="visa-logo"
+        />
+        <ul>
+            <li v-for="inputType in inputTypes">
+                <label v-bind:for="inputType.name">{{ inputType.label }}</label>
+                <input type="text"
+                       v-model="inputType.model"
+                       v-validate="inputType.validate"
+                       v-mask="inputType.mask"
+                       class="form-control"
+                       v-bind:name="inputType.name"
+                       v-bind:id="inputType.cssId"
+                       v-bind:placeholder="inputType.placeholder"
+                       :class="{ 'is-invalid': submitted && errors.has(inputType.name) }"
+                />
+                <span
+                        v-if="submitted && errors.has(inputType.name)"
+                        class="invalid-feedback"
+                >
+                    {{ errors.first(inputType.name) }}
+                </span>
+            </li>
+        </ul>
         <button type="submit" class="btn-submit">
-          <i class="fas fa-arrow-right"></i>
+            <i class="fas fa-arrow-right"></i>
         </button>
-      </div>
-    </div>
-  </form>
+    </form>
 </template>
 
 <script>
-import CreditCardImage from "../assets/images/visa-logo.png";
+    import CreditCardImage from "../assets/images/visa-logo.png";
 
-export default {
-  name: "CreditCardForm",
-  data: function() {
-    return {
-      creditCardImage: CreditCardImage,
-      creditCardAlt: "VISA",
-      masks: {
-        cardNumber: "####-####-####-####",
-        cardHolder: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        cardDateMonth: "##/##",
-        cardYear: "####",
-        cardCVV: "###"
-      },
-      card: {
-        cardNumber: "",
-        cardHolder: "",
-        cardDateMonth: "",
-        cardYear: "",
-        cardCVV: ""
-      },
-      submitted: false
-    };
-  },
-  methods: {
-    handleSubmit() {
-      this.submitted = true;
-      this.$validator.extend("verify_day_and_month", {
-        getMessage: `The day and month are not valid`,
-        validate: value =>
-          new Promise(resolve => {
-            const exDay = value.substr(0, 2);
-            const exMonth = value.substr(-2, 2);
-            resolve({
-              valid: exMonth < 13 && exDay < 32
-            });
-          })
-      });
-      this.$validator.extend("verify_year", {
-        getMessage: `The year is not valid`,
-        validate: value =>
-          new Promise(resolve => {
-            const currentDate = new Date();
-            resolve({
-              valid: value >= currentDate.getFullYear()
-            });
-          })
-      });
-      this.$validator.extend("verify_date", {
-        getMessage: `The expiration date cannot be in the past`,
-        validate: value =>
-          new Promise(resolve => {
-            const exDay = this.card.cardDateMonth.substr(0, 2);
-            const exMonth = this.card.cardDateMonth.substr(-2, 2);
-            const exYear = this.card.cardYear;
-            var today = new Date();
-            var someday = new Date();
-            someday.setFullYear(exYear, exMonth - 1, exDay);
-            resolve({
-              valid: value && someday > today
-            });
-          })
-      });
-      this.$validator.validate().then(valid => {
-        if (valid) {
-          alert(
-            "SUCCESS!! :-)\n\n" +
-              JSON.stringify([
-                this.card.cardNumber,
-                this.card.cardHolder,
-                this.card.cardDateMonth,
-                this.card.cardYear
-              ])
-          );
+    export default {
+        name: "CreditCardForm",
+        data: function () {
+            return {
+                creditCardImage: CreditCardImage,
+                creditCardAlt: "VISA",
+                inputTypes: [
+                    {
+                        model: "", validate: {required: true, min: 19, credit_card: true}, cssId: "cardNumber",
+                        mask: "####-####-####-####", name: "cardNumber", placeholder: "4111-1111-1111-1111", label: "Card Number"
+                    },
+                    {
+                        model: "", validate: {required: true, alpha_spaces: true},cssId: "cardHolder",
+                        mask: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX", name: "cardHolder", placeholder: "Sanjana Singh", label: "Cardholder Name"
+                    },
+                    {
+                        model: "", validate: {required: true, verify_day_and_month:true, verify_date:true},
+                        mask: "##/##", name: "cardMonth", placeholder: "01/02", cssId: "cardMonth", label: "Expiration"
+                    },
+                    {
+                        model: "", validate: {required: true, verify_date: true},
+                        mask: "####", name: "cardYear", placeholder: "2019", cssId: "cardYear", label: "    "
+                    },
+                    {
+                        model: "", validate: {required: true, digits: 3}, cssId: "cardCVV",
+                        mask: "###", name: "cardCVV", placeholder: "123", label: "CVV"
+                    },
+                ],
+                submitted: false
+            };
+        },
+        methods: {
+            handleSubmit() {
+                this.submitted = true;
+                this.$validator.extend("verify_day_and_month", {
+                    getMessage: `The day and month are not valid`,
+                    validate: value =>
+                        new Promise(resolve => {
+                            const exDay = document.getElementById("cardMonth").value.substr(0, 2);
+                            const exMonth = document.getElementById("cardMonth").value.substr(-2, 2);
+                            resolve({
+                                valid: exMonth < 13 && exDay < 32
+                            });
+                        })
+                });/*
+                this.$validator.extend("verify_year", {
+                    getMessage: `The year is not valid`,
+                    validate: value =>
+                        new Promise(resolve => {
+                            const currentDate = new Date();
+                            resolve({
+                                valid: value >= currentDate.getFullYear()
+                            });
+                        })
+                });*/
+                this.$validator.extend("verify_date", {
+                    getMessage: `The expiration date cannot be in the past`,
+                    validate: value =>
+                        new Promise(resolve => {
+                            const exDay = document.getElementById("cardMonth").value.substr(0, 2);
+                            const exMonth = document.getElementById("cardMonth").value.substr(-2, 2);
+                            const exYear = document.getElementById("cardYear").value;
+                            var today = new Date();
+                            var someday = new Date();
+                            someday.setFullYear(exYear, exMonth - 1, exDay);
+                            resolve({
+                                valid: value && someday > today
+                        });
+                        })
+                });
+                this.$validator.validate().then(valid => {
+                    if (valid) {
+                        var cardNumber = document.getElementById("cardNumber").value;
+                        var cardName = document.getElementById("cardHolder").value;
+                        var cardMonth = document.getElementById("cardMonth").value;
+                        var cardYear = document.getElementById("cardYear").value;
+
+                        alert(
+                            "SUCCESS!! :-)\n\n" +
+                            JSON.stringify([
+                                cardNumber, cardName, cardMonth, cardYear
+                            ])
+                        );
+                    }
+                });
+            }
         }
-      });
-    }
-  }
-};
+    };
 </script>
 
 <style scoped>
-.form-group {
-  margin: 15px 0 0;
-}
+    ul {
+        margin:0;
+        padding: 0;
+        padding-top: 20px;
+    }
+    li {
+        list-style: none;
+        padding-top: 10px;
+        margin-bottom: 10px;
+    }
+    li:first-child{
+        width:235px;
+    }
+    li:first-child input{
+        font-size: 20px;
+    }
+    li:nth-child(2){
+        width:235px;
+    }
+    li:nth-child(3){
+        width:80px;
+        float: left;
+    }
+    li:nth-child(4){
+        width:80px;
+        float: left;
+    }
+    li:nth-child(5){
+        width:100px;
+        margin-left: 10px;
+        float: left;
+    }
 </style>
--- tests/unit/creditcardform.spec.js
+++ tests/unit/creditcardform.spec.js
@@ -25,19 +25,20 @@ describe("Component CreditCardForm", () => {
     const cardNumber = wrapper.find("#cardNumber");
     cardNumber.element.value = "ABC"; //this is an invalid credit card number
     cardNumber.trigger("input");
-    expect(wrapper.vm.card.cardNumber).to.equal("");
+    expect(wrapper.vm.$el.querySelector("#cardNumber").value).to.equal("");
   });
 
   it("Doesnt allow more than 16 numbers on credit card number", () => {
     const cardNumber = wrapper.find("#cardNumber");
     cardNumber.element.value = "12345678987654321111111111"; //this is an invalid credit card number
     cardNumber.trigger("input");
-    expect(wrapper.vm.card.cardNumber).to.equal("1234-5678-9876-5432");
+    expect(wrapper.vm.$el.querySelector("#cardNumber").value).to.equal("1234-5678-9876-5432");
   });
 
   // Test Card Number - error generation on field
   it("Displays an error if validation fails with incorrect credit card number", async () => {
-    wrapper.setData({ card: { cardNumber: "1234-5678-9876-5432" } }); //this is an invalid credit card number
+    const cardNumber = wrapper.find("#cardNumber");
+    cardNumber.element.value = "1234-5678-9876-5432"; //this is an invalid credit card number
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -45,7 +46,8 @@ describe("Component CreditCardForm", () => {
   });
 
   it("Doesnt display an error if validation passes, when number starts from 4", async () => {
-    wrapper.setData({ card: { cardNumber: "4111-1111-1111-1111" } }); //this is a valid credit card number
+    const cardNumber = wrapper.find("#cardNumber");
+    cardNumber.element.value = "4111-1111-1111-1111"; //this is a valid credit card number
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -54,7 +56,8 @@ describe("Component CreditCardForm", () => {
 
   // Test Card Holder - error generation on field
   it("Displays an error if validation fails - name is empty", async () => {
-    wrapper.setData({ card: { cardHolder: " " } }); //this is an invalid credit card holder name
+    const cardHolder = wrapper.find("#cardHolder");
+    cardHolder.element.value =  " "; //this is an invalid credit card holder name
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -62,7 +65,8 @@ describe("Component CreditCardForm", () => {
   });
 
   it("Doesnt display an error if validation passes - alphabet only", async () => {
-    wrapper.setData({ card: { cardHolder: "Jane Smith" } }); //this is a valid credit card holder name
+    const cardHolder = wrapper.find("#cardHolder");
+    cardHolder.element.value = "Jane Smith"; //this is a valid credit card holder name
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -70,7 +74,8 @@ describe("Component CreditCardForm", () => {
   });
 
   it("Displays an error if validation fails with numbers", async () => {
-    wrapper.setData({ card: { cardHolder: "Jane 123" } }); //this is an invalid credit card holder name
+    const cardHolder = wrapper.find("#cardHolder");
+    cardHolder.element.value = "Jane 123"; //this is an invalid credit card holder name
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -78,7 +83,8 @@ describe("Component CreditCardForm", () => {
   });
 
   it("Displays an error if validation fails with special symbols &", async () => {
-    wrapper.setData({ card: { cardHolder: "Jane & Smith" } }); //this is an invalid credit card holder name
+    const cardHolder = wrapper.find("#cardHolder");
+    cardHolder.element.value = "Jane & Smith"; //this is an invalid credit card holder name
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -87,35 +93,40 @@ describe("Component CreditCardForm", () => {
 
   // Test Day, Month
   it("Doesnt allow more than 4 numbers on day and month", () => {
-    const cardDayMonth = wrapper.find("#DateMonth");
-    cardDayMonth.element.value = "011215487"; //this is an invalid credit card cvv
-    cardDayMonth.trigger("input");
-    expect(wrapper.vm.card.cardDateMonth).to.equal("01/12");
+    const cardMonth = wrapper.find("#cardMonth");
+    cardMonth.element.value = "011215487"; //this is an invalid credit card cvv
+    cardMonth.trigger("input");
+    expect(wrapper.vm.$el.querySelector("#cardMonth").value).to.equal("01/12");
   });
 
   it("Doesnt allow non-numerical values on credit card day and month", async () => {
-    wrapper.setData({ card: { cardDateMonth: "qq/qq" } }); //this is an invalid expiration date and month
+    const cardMonth = wrapper.find("#cardMonth");
+    cardMonth.element.value = "qq/qq"; //this is an invalid expiration date and month
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
-    expect(wrapper.vm.errors.has("DateMonth")).to.equal(true);
+    expect(wrapper.vm.errors.has("cardMonth")).to.equal(true);
   });
 
   it("Displays an error if day and month wrong", async () => {
-    wrapper.setData({ card: { cardDateMonth: "32/13" } }); //this is an invalid expiration date and month
+    const cardMonth = wrapper.find("#cardMonth");
+    cardMonth.element.value = "32/13"; //this is an invalid expiration date and month
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
-    expect(wrapper.vm.errors.has("DateMonth")).to.equal(true);
+    expect(wrapper.vm.errors.has("cardMonth")).to.equal(true);
   });
 
   it("Doesnt display an error if day and month correct", async () => {
     const currentYear = new Date();
-    wrapper.setData({ card: { cardDateMonth: "05/12", cardYear: currentYear.getFullYear()+1 } }); //this is a valid date and month
+    const cardMonth = wrapper.find("#cardMonth");
+    cardMonth.element.value = "05/12";
+    const cardYear = wrapper.find("#cardYear");
+    cardYear.element.value = currentYear.getFullYear()+1; //this is a valid date and month
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
-    expect(wrapper.vm.errors.has("DateMonth")).to.equal(false);
+    expect(wrapper.vm.errors.has("cardMonth")).to.equal(false);
   });
 
   //Test Year
@@ -123,10 +134,12 @@ describe("Component CreditCardForm", () => {
     const cardYear = wrapper.find("#cardYear");
     cardYear.element.value = "202456893"; //this is an invalid credit card year
     cardYear.trigger("input");
-    expect(wrapper.vm.card.cardYear).to.equal("2024");
+    expect(wrapper.vm.$el.querySelector("#cardYear").value).to.equal("2024");
   });
+
   it("Displays an error if year is expired", async () => {
-    wrapper.setData({ card: { cardYear: "2018" } }); //this is an invalid year
+    const cardYear = wrapper.find("#cardYear");
+    cardYear.element.value = "2018"; //this is an invalid year
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -134,7 +147,8 @@ describe("Component CreditCardForm", () => {
   });
 
   it("Doesnt display an error if year is up to date", async () => {
-    wrapper.setData({ card: { cardYear: "2022" } }); //this is a valid year
+    const cardYear = wrapper.find("#cardYear");
+    cardYear.element.value = "2022"; //this is a valid year
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -143,14 +157,15 @@ describe("Component CreditCardForm", () => {
 
   //Test CVV input
   it("Doesnt allow more than 3 numbers on credit card CVV", () => {
-    const cardCVV = wrapper.find("#cvv");
+    const cardCVV = wrapper.find("#cardCVV");
     cardCVV.element.value = "1234567"; //this is an invalid credit card cvv
     cardCVV.trigger("input");
-    expect(wrapper.vm.card.cardCVV).to.equal("123");
+    expect(wrapper.vm.$el.querySelector("#cardCVV").value).to.equal("123");
   });
 
   it("Displays an error if cardCVV is a string", async () => {
-    wrapper.setData({ card: { cardCVV: "qqq" } }); //this is an invalid expiration date and month
+    const cardCVV = wrapper.find("#cardCVV");
+    cardCVV.element.value = "qqq"; //this is an invalid expiration date and month
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     await flushPromises();
@@ -158,11 +173,12 @@ describe("Component CreditCardForm", () => {
   });
 
   // Test Submit button
-  it("Calls handleSubmit when submit button is clicked", () => {
+   it("Calls handleSubmit when submit button is clicked", () => {
     const clickMethodStub = sinon.stub();
     wrapper.setMethods({ handleSubmit: clickMethodStub });
     const btnSubmit = wrapper.find(".btn-submit");
     btnSubmit.trigger("click");
     expect(clickMethodStub.called).to.equal(true);
   });
+
 });
